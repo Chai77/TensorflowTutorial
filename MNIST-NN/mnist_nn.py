ef372890fbb03fe6049ac537c93ab79885e46dc1
Chaitanya Eranki <chaitanyaeranki@y7mail.com>
2020-07-04 16:46:54 -0500

    Other machine learning

--- MNIST-NN/mnist_nn.py
+++ MNIST-NN/mnist_nn.py
@@ -3,6 +3,7 @@ import keras
 from keras.models import Sequential
 from keras.layers import Flatten, Dense
 from keras.utils import normalize
+# import tensorflow as tf
 
 # Get the dataset from keras datasets
 mnist = keras.datasets.mnist
@@ -20,7 +21,7 @@ x_test = normalize(x_test, axis=1)
 
 # create the model: 2 hidden layers of 128 neurons and one output of 10 neurons
 # relu activation function for all of them
-model = keras.models.Sequential()
+model = Sequential()
 model.add(Flatten(input_shape=(28, 28)))
 model.add(Dense(units=128, activation='relu'))
 model.add(Dense(units=128, activation='relu'))
@@ -29,6 +30,18 @@ model.add(Dense(units=10, activation='softmax'))
 # compile all of the layers and add training parameters
 model.compile(optimizer='adam',
               loss="sparse_categorical_crossentropy", metrics=['accuracy'])
+
+def test(greeting, name):
+    """return a greeting
+
+    Args:
+        greeting (string): the name of the greeting
+        name (string): the name of the user
+
+    Returns:
+        string: a greeting created by adding greeting and name
+    """
+    return f"{greeting} {name}"
 
 # train the model
 model.fit(x_train, y_train, epochs=3)
