610acd6e793797c695dfb4b1fc8e96fb0b0fd88b
Chaitanya Eranki <chaitanyaeranki@y7mail.com>
2020-07-04 10:20:17 -0500

    added some comments

--- MNIST-NN/mnist_nn.py
+++ MNIST-NN/mnist_nn.py
@@ -1,6 +1,10 @@
 import numpy as np
 import keras
+from keras.models import Sequential
+from keras.layers import Flatten, Dense
+from keras.utils import normalize
 
+# Get the dataset from keras datasets
 mnist = keras.datasets.mnist
 (x_train, y_train), (x_test, y_test) = mnist.load_data()
 
@@ -10,20 +14,26 @@ y_train = y_train.astype(np.float32)
 x_test = x_test.astype(np.float32)
 y_test = y_test.astype(np.float32)
 
-x_train = keras.utils.normalize(x_train, axis=1)
-x_test = keras.utils.normalize(x_test, axis=1)
+# normalize x_train and y_train
+x_train = normalize(x_train, axis=1)
+x_test = normalize(x_test, axis=1)
 
+# create the model: 2 hidden layers of 128 neurons and one output of 10 neurons
+# relu activation function for all of them
 model = keras.models.Sequential()
-model.add(keras.layers.Flatten(input_shape=(28, 28)))
-model.add(keras.layers.Dense(units=128, activation='relu'))
-model.add(keras.layers.Dense(units=128, activation='relu'))
-model.add(keras.layers.Dense(units=10, activation='softmax'))
+model.add(Flatten(input_shape=(28, 28)))
+model.add(Dense(units=128, activation='relu'))
+model.add(Dense(units=128, activation='relu'))
+model.add(Dense(units=10, activation='softmax'))
 
+# compile all of the layers and add training parameters
 model.compile(optimizer='adam',
               loss="sparse_categorical_crossentropy", metrics=['accuracy'])
 
+# train the model
 model.fit(x_train, y_train, epochs=3)
 
+# evaluate the model that was completed
 loss, accuracy = model.evaluate(x_test, y_test)
 print(accuracy)
 print(loss)
