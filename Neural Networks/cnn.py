5774baa15697e91d7887ba1c7b0f8d37b74b9d74
Chaitanya Eranki <chaitanyaeranki@y7mail.com>
2020-07-05 12:37:44 -0500

    Made changes here

--- Neural Networks/cnn.py
+++ Neural Networks/cnn.py
@@ -1,15 +1,20 @@
 import tensorflow as tf
 
 from tensorflow.keras import datasets, layers, models
+from tensorflow.keras.optimizers import Adam
+
 import numpy as np
 
 (train_images, train_labels), (test_images,
                                test_labels) = datasets.cifar10.load_data()
 
-train_images = train_images.astype(np.float32)
-train_labels = train_labels.astype(np.float32)
-test_images = test_images.astype(np.float32)
-test_labels = test_labels.astype(np.float32)
+physical_devices = tf.config.list_physical_devices('GPU')
+tf.config.experimental.set_memory_growth(physical_devices[0], True)
+
+# train_images = train_images.astype(np.float32)
+# train_labels = train_labels.astype(np.float32)
+# test_images = test_images.astype(np.float32)
+# test_labels = test_labels.astype(np.float32)
 
 train_images, test_images = train_images / 255.0, test_images / 255.0
 
@@ -18,21 +23,22 @@ class_names = ['airplane', 'automobile', 'bird', 'cat',
 
 model = models.Sequential()
 model.add(layers.Conv2D(32, (3, 3), activation='relu', input_shape=(32, 32, 3)))
-# model.add(layers.MaxPooling2D((2, 2)))
-# model.add(layers.Conv2D(64, (3, 3), activation='relu'))
-# model.add(layers.MaxPooling2D((2, 2)))
-# model.add(layers.Conv2D(64, (3, 3), activation='relu'))
+model.add(layers.MaxPooling2D((2, 2)))
+model.add(layers.Conv2D(64, (3, 3), activation='relu'))
+model.add(layers.MaxPooling2D((2, 2)))
+model.add(layers.Conv2D(64, (3, 3), activation='relu'))
 
 model.add(layers.Flatten())
 model.add(layers.Dense(64, activation='relu'))
 model.add(layers.Dense(10))
 
+opt = Adam(learning_rate=0.0001)
 model.compile(optimizer='adam',
               loss=tf.keras.losses.SparseCategoricalCrossentropy(
                   from_logits=True),
               metrics=['accuracy'])
 
 history = model.fit(train_images, train_labels, epochs=10,
-                    validation_data=(test_images, test_labels), batch_size=32)
+                    validation_data=(test_images, test_labels), batch_size=256)
 
 model.save("weights.h5")
